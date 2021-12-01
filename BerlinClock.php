9307bf8f056df052179e8f0a26fb929a93692d46
maximeVa <maxime.vanachter@student.vinci.be>
2021-12-01 18:15:13 +0100

    add - test getBerlinMinutes given 0 returned 15x0

---
+++ BerlinClock.php
@@ -0,0 +1,8 @@
+<?php
+class BerlinClock {
+    public function getBerlinMinutes(int $minute): String
+    {
+        if($minute == 0)
+            return "OOOOOOOOOOO OOOO";
+    }
+}
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -1,9 +1,17 @@
 <?php
-
-
 use PHPUnit\Framework\TestCase;
 require 'BerlinClock.php';
 class BerlinClockTest extends TestCase
 {
+    public function testGetBerlinMinutes0ShouldReturn15x0()
+    {
+        // Arrange
+        $berlinClock = new BerlinClock();
 
+        // Act
+        $actual = $berlinClock->getBerlinMinutes(0);
+
+        // Assert
+        $this->assertEquals("OOOOOOOOOOO OOOO", $actual);
+    }
 }
